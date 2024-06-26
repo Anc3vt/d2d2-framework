8ddad8ee2f28253996af4c20de20a37ef166a818
Ancevt <me@ancevt.com>
2024-06-26 14:39:19 +0300

    BorderedRectangle on LineBatch

--- src/main/java/com/ancevt/d2d2/display/shape/BorderedRectangle.java
+++ src/main/java/com/ancevt/d2d2/display/shape/BorderedRectangle.java
@@ -19,152 +19,123 @@ package com.ancevt.d2d2.display.shape;
 
 import com.ancevt.d2d2.display.Color;
 import com.ancevt.d2d2.display.SimpleContainer;
+import lombok.Getter;
 
 public class BorderedRectangle extends SimpleContainer {
-	private static final Color DEFAULT_FILL_COLOR = Color.WHITE;
-	private static final Color DEFAULT_BORDER_COLOR = Color.BLACK;
+    private static final Color DEFAULT_FILL_COLOR = Color.WHITE;
+    private static final Color DEFAULT_BORDER_COLOR = Color.BLACK;
 
-	private static final float DEFAULT_WIDTH = 16;
-	private static final float DEFAULT_HEIGHT = 16;
+    private static final float DEFAULT_WIDTH = 16;
+    private static final float DEFAULT_HEIGHT = 16;
 
-	private final RectangleShape borderLeft;
-	private final RectangleShape borderRight;
-	private final RectangleShape borderTop;
-	private final RectangleShape borderBottom;
-	private final RectangleShape fillRect;
-	
-	private float borderWidth = 1;
-	
-	public BorderedRectangle() {
-		this(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_FILL_COLOR, DEFAULT_BORDER_COLOR);
-	}
+    private final RectangleShape fillRect;
 
-	public BorderedRectangle(float width, float height) {
-		this(width, height, DEFAULT_FILL_COLOR, DEFAULT_BORDER_COLOR);
-	}
-	
-	public BorderedRectangle(Color fillColor) {
-		this(DEFAULT_WIDTH, DEFAULT_HEIGHT, fillColor, DEFAULT_BORDER_COLOR);
-	}
+    @Getter
+    private final LineBatch lineBatch = new LineBatch();
 
-	public BorderedRectangle(Color fillColor, Color borderColor) {
-		this(DEFAULT_WIDTH, DEFAULT_HEIGHT, fillColor, borderColor);
-	}
-	
-	public BorderedRectangle(float width, float height, Color fillColor) {
-		this(width, height, fillColor, DEFAULT_BORDER_COLOR);
-	}
+    public BorderedRectangle() {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_FILL_COLOR, DEFAULT_BORDER_COLOR);
+    }
 
-	public BorderedRectangle(float width, float height, Color fillColor, Color borderColor) {
-		borderLeft   = new RectangleShape();
-		borderRight  = new RectangleShape();
-		borderTop    = new RectangleShape();
-		borderBottom = new RectangleShape();
-		fillRect     = new RectangleShape();
+    public BorderedRectangle(float width, float height) {
+        this(width, height, DEFAULT_FILL_COLOR, DEFAULT_BORDER_COLOR);
+    }
 
-		borderLeft.setSize(1, 1);
-		borderRight.setSize(1, 1);
-		borderTop.setSize(1, 1);
-		borderBottom.setSize(1, 1);
-		fillRect.setSize(1, 1);
-		
-		addChild(fillRect);
-		addChild(borderLeft);
-		addChild(borderRight);
-		addChild(borderTop);
-		addChild(borderBottom);
-		
+    public BorderedRectangle(Color fillColor) {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT, fillColor, DEFAULT_BORDER_COLOR);
+    }
 
-		setBorderColor(borderColor);
-		setFillColor(fillColor);
-		
-		setSize(width, height);
-	}
-	
-	public void setWidth(float width) {
-		fillRect.setWidth(width);
-		borderTop.setWidth(width);
-		borderBottom.setWidth(width);
-		borderRight.setX(width - borderWidth);
-	}
-	
-	public void setHeight(float height) {
-		fillRect.setHeight(height);
-		borderLeft.setHeight(height);
-		borderRight.setHeight(height);
-		borderBottom.setY(height - borderWidth);
-	}
-	
-	public void setSize(float width, float height) {
-		setWidth(width);
-		setHeight(height);
-	}
-	
-	@Override
-	public float getWidth() {
-		return fillRect.getWidth();
-	}
-	
-	@Override
-	public float getHeight() {
-		return fillRect.getHeight();
-	}
-	
-	public void setFillColor(Color color) {
-		if(color == null) {
-			if(fillRect.getParent() != null)
-				fillRect.removeFromParent();
-		} else {
-			if(fillRect.getParent() == null)
-				addChild(fillRect, 0);
-		}
-		
-		fillRect.setColor(color);
-	}
-	
-	public Color getFillColor() {
-		return fillRect.getColor();
-	}
-	
-	public void setBorderColor(Color color) {
-		if(color == null) {
-			if(borderLeft.getParent() != null) {
-				borderLeft.removeFromParent();
-				borderRight.removeFromParent();
-				borderTop.removeFromParent();
-				borderBottom.removeFromParent();
-			}
-		} else {
-			if(borderLeft.getParent() == null) {
-				addChild(borderLeft);
-				addChild(borderRight);
-				addChild(borderBottom);
-				addChild(borderTop);
-			}
-		}
-		
-		borderLeft.setColor(color);
-		borderRight.setColor(color);
-		borderTop.setColor(color);
-		borderBottom.setColor(color);
-	}
-	
-	public Color getBorderColor() {
-		return borderLeft.getColor();
-	}
-	
-	public void setBorderWidth(float borderWidth) {
-		this.borderWidth = borderWidth;
-		borderLeft.setWidth(borderWidth);
-		borderRight.setWidth(borderWidth);
-		borderTop.setHeight(borderWidth);
-		borderBottom.setHeight(borderWidth);
-		setSize(getWidth(), getHeight());
-	}
-	
-	public float getBorderWidth() {
-		return borderWidth;
-	}
+    public BorderedRectangle(Color fillColor, Color borderColor) {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT, fillColor, borderColor);
+    }
+
+    public BorderedRectangle(float width, float height, Color fillColor) {
+        this(width, height, fillColor, DEFAULT_BORDER_COLOR);
+    }
+
+    public BorderedRectangle(float width, float height, Color fillColor, Color borderColor) {
+        fillRect = new RectangleShape();
+
+        fillRect.setSize(1, 1);
+
+        addChild(fillRect);
+
+        setFillColor(fillColor);
+        setBorderColor(borderColor);
+
+        setSize(width, height);
+        addChild(lineBatch);
+    }
+
+    public void setWidth(float width) {
+        fillRect.setWidth(width);
+        rebuildBorders();
+    }
+
+    public void setHeight(float height) {
+        fillRect.setHeight(height);
+        rebuildBorders();
+    }
+
+    public void setSize(float width, float height) {
+        fillRect.setWidth(width);
+        fillRect.setHeight(height);
+        rebuildBorders();
+    }
+
+    @Override
+    public float getWidth() {
+        return fillRect.getWidth();
+    }
+
+    @Override
+    public float getHeight() {
+        return fillRect.getHeight();
+    }
+
+    public void setFillColor(Color color) {
+        if (color == null) {
+            if (fillRect.getParent() != null)
+                fillRect.removeFromParent();
+        } else {
+            if (fillRect.getParent() == null)
+                addChild(fillRect, 0);
+        }
+
+        fillRect.setColor(color);
+    }
+
+    public Color getFillColor() {
+        return fillRect.getColor();
+    }
+
+    public void setBorderColor(Color color) {
+        lineBatch.setVisible(color != null);
+        if (color != null) {
+            lineBatch.setColor(color);
+        }
+    }
+
+    public Color getBorderColor() {
+        return lineBatch.getColor();
+    }
+
+    public void setBorderWidth(float borderWidth) {
+        getLineBatch().setLineWidth(borderWidth);
+    }
+
+    public float getBorderWidth() {
+        return lineBatch.getLineWidth();
+    }
+
+    private void rebuildBorders() {
+        lineBatch.getLines().clear();
+        lineBatch.moveTo(0, 0);
+        lineBatch.lineTo(getWidth(), 0);
+        lineBatch.lineTo(getWidth(), getHeight());
+        lineBatch.lineTo(0, getHeight());
+        lineBatch.closePath();
+    }
 }
 
 
