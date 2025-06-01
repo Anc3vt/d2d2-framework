27469a1dd539a18f3d0b931b3ff0338e750959c4
Ancevt <me@ancevt.com>
2025-06-01 11:32:43 +0600

    TextureEngine old methods deprecation

--- src/main/java/com/ancevt/d2d2/scene/texture/TextureEngine.java
+++ src/main/java/com/ancevt/d2d2/scene/texture/TextureEngine.java
@@ -2,13 +2,13 @@
  * Copyright (C) 2025 the original author or authors.
  * See the notice.md file distributed with this work for additional
  * information regarding copyright ownership.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,10 +25,16 @@ import java.io.InputStream;
 // TODO: refactor and delete this interface
 public interface TextureEngine {
 
+    @Deprecated
+        //TODO: delete
     boolean bind(Texture texture);
 
+    @Deprecated
+        //TODO: delete
     void enable(Texture texture);
 
+    @Deprecated
+        //TODO: delete
     void disable(Texture texture);
 
     Texture createTexture(InputStream pngInputStream);
